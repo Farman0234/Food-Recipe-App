b8d195108eb174de5b89d8ac4bb63baeb8579115
Farman Anees <farmananees831@gmail.com>
2025-08-14 21:15:15 +0500

    Updated code with new changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import Mainpage from "./Components/Mainpage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Mealinfo from "./Components/Mealinfo";
-import FunnyErrorPage from "./Components/FunnyErrorPage";
 
 function App() {
   return (
@@ -11,8 +10,7 @@ function App() {
         <Route path="/" element={<Mainpage />} />
         <Route path="/:mealid" element={<Mealinfo />} />z
       </Routes>
-      
-      </>
+    </>
   );
 }
 
