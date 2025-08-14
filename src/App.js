4fdbe39ff3ece5e79cb3887feea9f23f0850f1a7
Farman Anees <farmananees831@gmail.com>
2025-08-14 21:25:20 +0500

    Updated code with new change ss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Mainpage from "./Components/Mainpage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {Route, Routes } from "react-router-dom";
 import Mealinfo from "./Components/Mealinfo";
 
 function App() {
